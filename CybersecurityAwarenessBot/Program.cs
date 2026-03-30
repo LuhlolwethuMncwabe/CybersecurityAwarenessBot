062968bdd28d19b52ead6b2cca6e2d21bb41688f
Luhlolwethu Mncwabe <263221050+LuhlolwethuMncwabe@users.noreply.github.com>
2026-03-31 00:12:36 +0200

    cleaning up the program.cs structure.

--- CybersecurityAwarenessBot/Program.cs
+++ CybersecurityAwarenessBot/Program.cs
@@ -12,21 +12,26 @@ namespace CybersecurityAwarenessBot
     {
         static void Main(string[] args)
         {
-            // Launch greeting
-            Console.WriteLine("Cybersecurity Awareness Bot is Starting...");
+           
             ConsoleUI.DisplayLogo();
+
             AudioGreeting.GreetWelcome();
 
             ConsoleUI.PrintHeader("Main Menu");
 
-            // Get user name
+
             string userName = ConsoleUI.GetUserName();
             ConsoleUI.DisplayWelcome(userName);
 
-            // Hand off to ChatBot class to handle the full conversation
+
             ChatBot bot = new ChatBot(userName);
             bot.Run();
 
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n  Thank you for using the Cybersecurity Awareness Bot!");
+            Console.WriteLine(" \n Remember to stay safe online. Goodbye!");
+            Console.ResetColor();
+            ConsoleUI.PrintDivider();
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
         }
